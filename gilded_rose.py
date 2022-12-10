da07b5a9b5add0e0dcccdd8e75b2dbd022963fa8
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-12-10 00:20:55 -0500

    Conjured items
    Incorporación de un nuevo tipo de item , items conjurados

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,26 +5,56 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def __increase_quality_backstage(self,item,passes_backstage):
+        if (item.name == passes_backstage and item.sell_in < 11 and item.quality< 50):
+            item.quality = item.quality + 1
+        if (item.name == passes_backstage and item.sell_in < 6 and item.quality< 50):
+            item.quality = item.quality + 1
+
+    def __decrease_sell_in_no_sulfuras(self,item,sulfuras):
+        if item.name != sulfuras:
+            item.sell_in = item.sell_in - 1
+
+    def __lead_conjured_items(self,item,conjured):
+        if item.name == conjured and 0<item.sell_in<150:
+           
+            item.sell_in = item.sell_in - 1
+            item.quality=item.quality-2
+            return True
+        elif(item.sell_in<0):
+            item.sell_in=item.sell_in-1
+            item.quality=item.quality-4
+            return True
+        else :
+            return False
+            
+    def __is_passes_or_aged_brie(self,item,aged_brie,passes_backstage):
+        return item.name==aged_brie or item.name==passes_backstage
+
     def update_quality(self):
         passes_backstage="Backstage passes to a TAFKAL80ETC concert"
         sulfuras="Sulfuras, Hand of Ragnaros"
         aged_brie="Aged Brie"
+        conjureds="Conjured item"
+        
         item_names=(passes_backstage,sulfuras,aged_brie)
         
         for item in self.items:
 
-            if item.name not in item_names and item.quality > 0 :
+            if self.__lead_conjured_items(item,conjureds):
+                continue
+
+            if  item.name not in item_names and item.quality > 0:
                 item.quality = item.quality - 1
-            elif item.quality < 50 and item.name==aged_brie or item.name==passes_backstage :
+            elif item.quality < 50 and self.__is_passes_or_aged_brie(item,aged_brie,passes_backstage) :
                 item.quality = item.quality + 1
                 
-                if (item.name == passes_backstage and item.sell_in < 11 and item.quality< 50):
-                    item.quality = item.quality + 1
-                if (item.name == passes_backstage and item.sell_in < 6 and item.quality< 50):
-                    item.quality = item.quality + 1
+                
+                self.__increase_quality_backstage(item,passes_backstage)
 
-            if item.name != sulfuras:
-                item.sell_in = item.sell_in - 1
+
+            self.__decrease_sell_in_no_sulfuras(item,sulfuras)
+
             if item.sell_in < 0 and item.quality>0 and item.name not in item_names :
                 item.quality = item.quality - 1
             elif(item.sell_in < 0 and item.name == passes_backstage):
@@ -32,7 +62,7 @@ class GildedRose(object):
                 
             elif (item.sell_in < 0 and  item.name == aged_brie and item.quality < 50):
                 item.quality = item.quality + 1
-
+    
 
 class Item:
     def __init__(self, name, sell_in, quality):
